de3980df02d3c90a9e9485ab4c6f09c0823beaf0
NoelleIsThaBomb.com <noelleclement@hotmail.com>
2018-05-14 13:36:49 +0200

    progress ass 1

--- ass1/input_output.cpp
+++ ass1/input_output.cpp
@@ -1,16 +1,3 @@
-/*
-    TODO:
-        char to morse (with same technique as decodeMorse)
-        check if rest of code needs reorganization
-        raspberry pi setup
-        connect pi to program
-
-
-
-*/
-
-
-
 #include <stdlib.h>
 #include <string>
 #include <iostream>
@@ -21,84 +8,49 @@
 
 using namespace std;
 
-//decodeMorse: incoming morse 'character string' to regular character
 char decodeMorse(string s) {
-    
-    
-    /*
-    original string (without numbers and punctuation marks): 
-
-    const string morse("EISHVUF*ARL*WPJTNDBXKCYMGZQO");
-    int p = -1;
-  
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '-') {
-            p += (1 << (4 - i));
-        }
-        else p++;
-    }
-    return morse[p];
-
-    */
-
-
-
-    /*
-    notes:
-        morse tree (string) order is based on the order of .'s and -'s, fe E=., I=.., S=...
-        added characters add one 'layer' to tree, so for bitwise shift 4 becomes 5 
-        non-ascii table characters are added as *
-        spaces in tree are _
-    */
-
-    const string morse("EISH54V*3UF*_**2ARL_**+_WP**J*1TNDB6=X/_KC*_Y*_MGZ7_Q**O*8_*90"); //"EISH54V(´S)3UF(É)_(Ü)(-D)2ARL_(È)(Ä)+_WP('P)(À)J(^J)1   TNDB6=X/_KC(,C)_Y(^H)_MGZ7_Q(^G)(~N)O(Ö)8_(CH)90" 
-    int p = -1;
-  
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '-') {
-            p += (1 << (5 - i));
-        }
-        else p++;
-    }
-    return morse[p];
-
-
+  const string morse("EISHVUF*ARL*WPJTNDBXKCYMGZQO");
+  int p = -1;
+  for (int i = 0; i < s.length(); i++) {
+    if (s[i] == '-') p += 1 << (4 - i);
+    else p++;
+  }
+  return morse[p];
 }
 
 void switchChanged() {
-    static string buffer;
-    static unsigned int previousTime = 0, currentTime, timePassed;
-    currentTime = millis();
-    if (previousTime == 0) { // first pulse
-        previousTime = currentTime;
-        return;
-    }
-    timePassed = currentTime - previousTime;
+  static string buffer;
+  static unsigned int previousTime = 0, currentTime, timePassed;
+  currentTime = millis();
+  if (previousTime == 0) { // first pulse
     previousTime = currentTime;
-    if (digitalRead(INPUT_PIN) == LOW) { // switch pressed
-        if (timePassed > 200) {
+    return;
+  }
+  timePassed = currentTime - previousTime;
+  previousTime = currentTime;
+  if (digitalRead(INPUT_PIN) == LOW) { // switch pressed
+    if (timePassed > 200) {
 
-        // TO DO
+      // TO DO
 
-        }
-        if (timePassed > 600) {
-
-        // TO DO
-
-        }
-    } 
-    else { // switch released
-        if (timePassed > 200) {
-
-        // TO DO
-
-        }
-        else {
-
-        // TO DO
-
-        }
     }
+    if (timePassed > 600) {
+
+      // TO DO
+
+    }
+  } else { // switch released
+    if (timePassed > 200) {
+
+      // TO DO
+
+    }
+    else {
+
+      // TO DO
+
+    }
+  }
 }
 
 void wait(int n) {
